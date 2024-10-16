e9b49ff75777a1c5d1963400251c9e05c106f3ea
MeAndrew24 <a01253138@tec.mx>
2024-10-16 11:38:26 -0700

    Added no props profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,72 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-function Profile() {
+function Profile(props) {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>This is your profile!</Text>
+    <View style={{ flex: 1 }}>
+      <View style={styles.arriba}>
+        <View style={[styles.profilePic, { backgroundColor: "black" }]}>
+          <Text style={styles.capitalLetter}>S</Text>
+        </View>
+
+        <View
+          style={{
+            justifyContent: "space-between",
+            flexDirection: "row",
+            width: "52%",
+            marginTop: 10,
+          }}
+        >
+          <View style={{ flexDirection: "row" }}>
+            <Text>Followers: </Text>
+            <Text>1 </Text>
+            {/* Display the number of followers with props in future */}
+          </View>
+
+          <View style={{ flexDirection: "row" }}>
+            <Text>Following: </Text>
+            <Text>1</Text>
+            {/* Display the number of following users with props in future */}
+          </View>
+        </View>
+      </View>
+      <View style={styles.abajo}>
+        <Text style={styles.subtitle}>Posts</Text>
+      </View>
     </View>
   );
 }
 
 export default Profile;
+
+const styles = StyleSheet.create({
+  arriba: {
+    backgroundColor: "#fbfbfb",
+    width: "100%",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profilePic: {
+    borderRadius: 64,
+    justifyContent: "center",
+    height: 64,
+    width: 64,
+  },
+  subtitle: {
+    fontSize: 20,
+    marginLeft: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    fontWeight: "400",
+  },
+  abajo: {
+    width: "100%",
+    flex: 3.5,
+  },
+  capitalLetter: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+});
