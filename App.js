41840e76905e44b077bf9f5928a783eee0750869
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-10-15 17:48:39 -0700

    Patch: NavTab inside of NavigationContainer

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Navlog />
+      <NavTab />
     </NavigationContainer>
   );
 }
