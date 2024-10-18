7b36117a8dd5bd3581dc25ba7fa3c5d08bfa459f
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-10-18 13:21:26 -0700

    Patch: Better Naming Conventions

--- src/screens/MainNavigator.js
+++ src/screens/MainNavigator.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import { useLogin } from "../context/LoginProvider";
-import NavTab from "./NavTab";
-import Navlog from "./Navlog";
+import NavMain from "./NavMain";
+import NavLogin from "./NavLogin";
 
 const MainNavigator = () => {
   const { isLoggedIn } = useLogin();
-  return isLoggedIn ? <NavTab /> : <Navlog />;
+  return isLoggedIn ? <NavMain /> : <NavLogin />;
 };
 
 export default MainNavigator;
